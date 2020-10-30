7608671109386328e17965061591111a8e8a7e0f
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-10-30 15:41:08 -0300

    Added model, factory, seeder, resource controller and migration for Providers

---
+++ database/factories/ProviderFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Provider;
+use Faker\Generator as Faker;
+
+$factory->define(Provider::class, function (Faker $faker)
+{
+	return [
+	   'name'    => $faker->company,
+	   'address' => $faker->streetAddress,
+	];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
 	{
 		$this->call(UsersSeeder::class);
 		$this->call(ClientSeeder::class);
+		$this->call(ProviderSeeder::class);
 	}
 }
